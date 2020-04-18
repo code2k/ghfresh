2be07969bac7cb4fd01bf92c237717b0196fb83a
Andreas Pataki <andreas.pataki@code2k.net>
2020-04-18 20:04:45 +0200

    Updates

---
+++ precache-manifest.7ebbea7f6a57c154d1bee25ebadfe6de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1d20b18752dbea90ea11b4b1e1c61c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61404dbbed5471471bca",
+    "url": "./static/js/2.f6c559fd.chunk.js"
+  },
+  {
+    "revision": "33fddf4d4d617331ba0acf6de38b719c",
+    "url": "./static/js/2.f6c559fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197ae92ab8db5224c816",
+    "url": "./static/js/main.92083243.chunk.js"
+  },
+  {
+    "revision": "2babc48951f6cf0bebb2",
+    "url": "./static/js/runtime-main.4f6425ec.js"
+  }
+]);
